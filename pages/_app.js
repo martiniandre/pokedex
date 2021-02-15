ab617b3534c96c7f88b0b7bf7be61b0671dd2fcd
Andre <andremartini.10@gmail.com>
2021-02-15 16:25:13 -0300

    Remove service-worker config

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,12 @@
-import { useEffect } from 'react'
 import '../styles/globals.css'
-import Head from 'next/head'
 
 
-const CommonHead = () => {
-  useEffect(()=> {
-    register('/service-worker.js', {scope: '/'}) 
-    return () => {
-        unregister();  
-    };
-}, [])
-  return (
-        <Head>
-            <title>Next PWA demo</title>
-            <link rel="manifest" href="/static/manifest.webmanifest" />
-            <meta name = "apple-mobile-web-app-capable" content="yes" />
-            <meta name = "apple-mobile-web-app-status-bar-style" content="black" />
-            <meta name = "apple-mobile-web-app-title" content="Next PWA demo" />
-            <link rel="apple-touch-icon" href="/static/logo-icon-144x144.png" />
-            <link rel="apple-touch-startup-icon" href="/static/logo-icon-144x144.png" />
-        </Head>
-    )
-}
+
 
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-    <CommonHead/>
-    <Component {...pageProps}/>
-    </>
-  ) 
+  return <Component {...pageProps}/>
+
 }
 
 export default MyApp
